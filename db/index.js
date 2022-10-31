ca39721ea5b565b9a9a103f784e0b898044aba4d
Edward Yuabov <yuaboved@gmail.com>
2022-10-31 15:01:05 -0400

    Installed packages

---
+++ db/index.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Successfully connected to MongoDB')
+  })
+  .catch((e) => {
+    console.log('Connection Failed', e.messages)
+  })
+const db = mongoose.connection
+module.exports = db
---
+++ server.js
@@ -0,0 +1,10 @@
+const PORT = process.env.PORT || 3001
+const db = require('./db')
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.listen(PORT, () => {
+  console.log(`Express server is listening on port ${PORT}`)
+})
