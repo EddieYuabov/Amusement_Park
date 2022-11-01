45b216c14f2f75ec09fdb5c0db746025df4f266e
Marissa Dilig <hello.maripd@gmail.com>
2022-11-01 09:29:39 -0500

    created ride cards, title and thrill level

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,30 @@
 import logo from './logo.svg'
 import './App.css'
+import React from 'react'
+// function App() {
+//   return
+// }
+// export default App
+import Rides from './components/Rides'
+import RidesArray from './components/RidesArray.js'
 
-function App() {
-  return
+const App = () => {
+  return (
+    <div className="main-container">
+      <ul className="rides-list">
+        {RidesArray.map((rideItem) => {
+          console.log(rideItem)
+          {
+          }
+          return (
+            <Rides ride={rideItem.ride} thrillLevel={rideItem.thrillLevel} />
+          )
+        })}
+      </ul>
+      {/* <Route path="/" element={<App />} />
+      <Route path="/Rides/" element={<Rides />} /> */}
+    </div>
+  )
 }
 
 export default App
